b6413cecdd0f2f647ba22fc8d065d827e4bbb456
SpyDev14 <vlad.buklovsky@gmail.com>
2025-03-17 17:05:07 +0400

    Namespace Base Server.cs has been replaced with a shorter version.

--- Content.Core/Saver/BaseSaver.cs
+++ Content.Core/Saver/BaseSaver.cs
@@ -1,57 +1,53 @@
-﻿using System.IO;
-
-namespace Content.Core.Saver
+﻿namespace Content.Core.Saver;
+internal class BaseSaver(SaveData data) : IFileSaver
 {
-    internal class BaseSaver(SaveData data) : IFileSaver
+    private readonly SaveData _data = data;
+
+    public void Save(BaseFile file)
     {
-        private readonly SaveData _data = data;
+        string fullPath = _data.OutputPath + $"\\{_data.AssembledFileName}.yml";
 
-        public void Save(BaseFile file)
+        int index = 1;
+        while (File.Exists(fullPath))
         {
-            string fullPath = _data.OutputPath + $"\\{_data.AssembledFileName}.yml";
-
-            int index = 1;
-            while (File.Exists(fullPath))
+            switch (_data.OnFileAlreadyExistOperation)
             {
-                switch (_data.OnFileAlreadyExistOperation)
-                {
-                    case OnFileAlreadyExist.Overwrite:
-                        File.Delete(fullPath);
-                        break;
+                case OnFileAlreadyExist.Overwrite:
+                    File.Delete(fullPath);
+                    break;
 
-                    case OnFileAlreadyExist.CreateWithIndex:
-                        fullPath = _data.OutputPath + $"\\{_data.AssembledFileName} ({index}).yml";
-                        index++;
-                        break;
+                case OnFileAlreadyExist.CreateWithIndex:
+                    fullPath = _data.OutputPath + $"\\{_data.AssembledFileName} ({index}).yml";
+                    index++;
+                    break;
 
-                    case OnFileAlreadyExist.ThrowException:
-                        throw new FileAlreadyExistsException($"File {_data.AssembledFileName}.yml in {_data.OutputPath} already exist");
-                }
-            }
-
-            using (FileStream fileStream = File.Create(fullPath))
-            {
-                using (StreamWriter writer = new StreamWriter(fileStream))
-                {
-                    writer.Write(file.FileContent);
-                }
+                case OnFileAlreadyExist.ThrowException:
+                    throw new FileAlreadyExistsException($"File {_data.AssembledFileName}.yml in {_data.OutputPath} already exist");
             }
         }
 
-        public bool TrySave(BaseFile file, out Exception? exception)
+        using (FileStream fileStream = File.Create(fullPath))
         {
-            try
+            using (StreamWriter writer = new StreamWriter(fileStream))
             {
-                Save(file);
+                writer.Write(file.FileContent);
             }
-            catch (Exception e)
-            {
-                exception = e;
-                return false;
-            }
-
-            exception = null;
-            return true;
         }
+    }
+
+    public bool TrySave(BaseFile file, out Exception? exception)
+    {
+        try
+        {
+            Save(file);
+        }
+        catch (Exception e)
+        {
+            exception = e;
+            return false;
+        }
+
+        exception = null;
+        return true;
     }
 }
