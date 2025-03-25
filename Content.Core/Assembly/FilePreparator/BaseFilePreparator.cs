b2c7361a453ffd1c6a499d5e4281b277185e5a9e
SpyDev14 <vlad.buklovsky@gmail.com>
2025-03-25 10:54:27 +0400

    Remove unused usings

--- Content.Core/Assembly/FilePreparator/BaseFilePreparator.cs
+++ Content.Core/Assembly/FilePreparator/BaseFilePreparator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Content.Core.Assembly.FilePreparator
+﻿namespace Content.Core.Assembly.FilePreparator
 {
     internal class BaseFilePreparator(AssemblyData data) : IFilePreparator
     {
--- Content.Core/Saver/SaveData.cs
+++ Content.Core/Saver/SaveData.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Content.Core.Saver
+﻿namespace Content.Core.Saver
 {
     internal class SaveData(string outPath, string assembledFileName)
     {
